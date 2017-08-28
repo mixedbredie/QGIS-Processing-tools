992df7093d861c5f360f291c828213142da6abad
Ross McDonald <github@mixedbredie.net>
2017-08-28 12:00:28 +0100

    update ImportVector line

--- 1.1/scripts/find_similar_line_feature.py
+++ 1.1/scripts/find_similar_line_feature.py
@@ -13,7 +13,7 @@
 #==================================
 from qgis.core import *
 from PyQt4.QtCore import *
-from processing.core.VectorWriter import VectorWriter
+from processing.tools.vector import VectorWriter
 import processing 
 from scipy.spatial.distance import cdist
 import numpy as np
